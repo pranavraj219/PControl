8eda2c480bf61e065ad9f6edb8e3afab4b7734a0
Pranav Raj <pranavraj219@gmail.com>
2017-03-13 01:11:27 +0530

    Added Output redirection to user

--- MAIN.py
+++ MAIN.py
@@ -39,7 +39,7 @@ while(1):
     result, data = M.uid('fetch', latest_email_uid, '(RFC822)')
     raw_email = data[0][1]
     email_message = email.message_from_bytes(raw_email)
-    def get_first_text_block(email_message_instance):
+    def get_text_block(email_message_instance):
         maintype = email_message_instance.get_content_maintype()
         if maintype == 'multipart':
             for part in email_message_instance.get_payload():
@@ -49,14 +49,20 @@ while(1):
             return email_message_instance.get_payload()
     
     if(prev_id!=str(latest_email_uid,'utf-8')):
-     From= email.utils.parseaddr(email_message['From'])[1]
-     msg=get_first_text_block(email_message)
+     #From= email.utils.parseaddr(email_message['From'])[1]
+     msg=get_text_block(email_message)
      print("Command -\n"+msg)
+     #if(msg.find("exit")):
+      #   print(msg)
+       #  break
      p = subprocess.Popen(msg, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
      OUT="Output for "+"\""+msg+"\" :-\n"
+     stats="\nExecution incomplete"
      for line in p.stdout.readlines():
        OUT=OUT+line.decode('utf-8')
-     smtpObj.sendmail(cmail,uemail,'Subject: '+msg+'\n'+OUT)
+     if(p.poll()==None):
+         stats="\nExecution complete"
+     smtpObj.sendmail(cmail,uemail,'Subject: '+msg+'\n'+OUT+stats)
      prev_id=str(latest_email_uid,'utf-8')
 smtpObj.quit()
 M.close()
