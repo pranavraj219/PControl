500e19d063e3bac0ba4b0fdc68c2cffd752e3ffc
Pranav Raj <pranavraj219@gmail.com>
2017-03-13 01:37:16 +0530

    Add files via upload
    Added documentation

--- MAIN.py
+++ MAIN.py
@@ -13,8 +13,8 @@ while(1):
     try:
         cmail = input("Email address for PC:- ")
         password = getpass.getpass()
-        M.login(cmail, password)
-        smtpObj.ehlo()
+        M.login(cmail, password)            #Establishing IMAP connection with the server
+        smtpObj.ehlo()                      #Establishing SMTP connection with the server
         smtpObj.login(cmail,password)
         print("Logged in Successfully as "+cmail)
         break
@@ -28,10 +28,11 @@ except IOError:
     prev_id=-1
 while(1):
     fo_obj=open("ID.txt","w")
+    #Using previous ID to check the repetition of a single mail executing more than once
     fo_obj.write(str(prev_id))
     fo_obj.close()
     wait_sec(5)
-    result, data = M.uid('search', None, 'FROM '+uemail) # search and return uids
+    result, data = M.uid('search', None, 'FROM '+uemail) #search and return uids
     st=str(data[0],'utf-8')
     if(len(st)<=0):
         continue
@@ -49,12 +50,9 @@ while(1):
             return email_message_instance.get_payload()
     
     if(prev_id!=str(latest_email_uid,'utf-8')):
-     #From= email.utils.parseaddr(email_message['From'])[1]
+#Parsing E-mail and executing command
      msg=get_text_block(email_message)
      print("Command -\n"+msg)
-     #if(msg.find("exit")):
-      #   print(msg)
-       #  break
      p = subprocess.Popen(msg, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
      OUT="Output for "+"\""+msg+"\" :-\n"
      stats="\nExecution incomplete"
@@ -62,6 +60,7 @@ while(1):
        OUT=OUT+line.decode('utf-8')
      if(p.poll()==None):
          stats="\nExecution complete"
+#Sending Output to the user
      smtpObj.sendmail(cmail,uemail,'Subject: '+msg+'\n'+OUT+stats)
      prev_id=str(latest_email_uid,'utf-8')
 smtpObj.quit()
